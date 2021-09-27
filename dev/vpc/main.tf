c60bce974fae2c728372ca5d98252c6bf7ffd816
esnow <edward.snow@disneystreaming.com>
2021-09-27 23:42:22 +0100

    update structure

--- dev/vpc/main.tf
+++ dev/vpc/main.tf
@@ -6,8 +6,23 @@ provider "aws" {
 }
 
 module "eu-west-2" {
-  source = "../../../modules/vpc"
-  region = "us-east-2"
+  source = "../../modules/vpc"
+  region = "eu-west-2"
+  
+  private_subnet_a = "10.0.1.0/24"
+  private_subnet_b = "10.0.2.0/24"
+  public_subnet_a = "10.0.255.0/24"
+  public_subnet_b = "10.0.254.0/24"
+
+  ssh_security_group = ["22"]
+  web_security_group = ["80", "443"]
+
+  app = "fargate-poc-vpc"
+}
+
+module "eu-west-1" {
+  source = "../../modules/vpc"
+  region = "eu-west-1"
   
   private_subnet_a = "10.0.1.0/24"
   private_subnet_b = "10.0.2.0/24"
